c6ac3a0f52ad05c7e86d400296212a89aece4877
Francisco Albuquerque <92957206+Sblck@users.noreply.github.com>
2025-07-03 12:15:31 +0100

    Update app.py
    Obter a metereologia (obter_metereologia) reutilizando a função obter_localizacao_via_ip()
    
    Refactorização dos metodos obter_localizacao_via_ip e obter_metereologia via um metodo generico de pedido (url_request)
    
    Alteração do for loop principal para suportar quando a resposta existe no dicionario de respostas apontar para uma função ( anteriormente apontavamos para a função mas como estava definido o valor era atribuido na definição inicial do dicionário, agora apenas aponta para a função responsável)

--- app.py
+++ app.py
@@ -3,38 +3,68 @@ from datetime import datetime
 
 def obter_resposta(texto: str) -> str:
 
+
+    def url_request(url: str) -> str:
+        try:
+            with urllib.request.urlopen(url) as response:
+                return response.read().decode()
+        except Exception as e:
+            return f'Desculpa, não consegui obter a informação! {e}'
+
     def obter_localizacao_via_ip() -> dict:
         try:
             url: str = "http://ip-api.com/json/"
-            with urllib.request.urlopen(url) as response:
-                if response:
-                    data = json.loads(response.read().decode())
-                    return data['city'], data['country']
-                else:
-                    return f'Desculpa, não consegui obter a localização!'
+            resposta = url_request(url)
+            data = json.loads(resposta)
+            return data['city'], data['country']
         except Exception as e:
             return f'Desculpa, não consegui obter a localização! {e}'
         
-    def descompactar(valor: tuple, tamanho: int, template: str) -> str:
-        if isinstance(valor, tuple) and len(valor) == tamanho:
+    def obter_metereologia() -> dict:
+        '''
+        Obter a metereologia da localização do utilizador
+        Fonte: https://wttr.in
+        Exemplo: http://wttr.in/Lisboa?format=3
+        Referencia: https://github.com/chubin/wttr.in
+        Formato:
+        '''
+        try:
+            url = f"http://wttr.in/{obter_localizacao_via_ip()[0]}?format=3"
+            data = url_request(url)
+            return data
+        except Exception as e:
+            return f'Desculpa, não consegui obter a metereologia! {e}'
+        
+    def descompactar(valor: tuple, template: str) -> str:
+        if isinstance(valor, tuple):
             return template.format(*valor)
-        return valor
+        
+
+    def resposta_localizacao() -> str:
+        return f'{descompactar(obter_localizacao_via_ip(), "Estás na cidade de {} em {}")}'
+    
+    
+    def resposta_metereologia() -> str:
+        return f'{obter_metereologia()}'
 
     respostas = {
          ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
          'como estás': 'Estou bem, obrigado!',
          ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
-         ('minha localização', 'onde estou'): descompactar(obter_localizacao_via_ip(), 2, "Estás na cidade de {} em {}"),
+         ('minha localização', 'onde estou'): resposta_localizacao,
+         ('tempo', 'metereologia'): resposta_metereologia,
      }
+    
+    def processar_resposta(resposta):
+        return resposta() if callable(resposta) else resposta
 
     comando: str = texto.lower()
 
     for chave, resposta in respostas.items():
-         if isinstance(chave, tuple):
-             if comando in chave:
-                 return resposta
-         elif chave in comando:
-            return resposta
+        if isinstance(chave, tuple) and any(k in comando for k in chave):
+            return processar_resposta(resposta)
+        elif isinstance(chave, str) and chave in comando:
+            return processar_resposta(resposta)
          
     if 'horas' in comando:
         return f'São: {datetime.now():%H:%M} horas'
